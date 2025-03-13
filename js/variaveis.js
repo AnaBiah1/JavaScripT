6ab70a421683194415fd98f223a88ae4ca7a175d
AnaBiah1 <anasantos.dev2024@gmail.com>
2025-03-13 11:13:26 -0300

    Exercicio 5

---
+++ js/variaveis.js
@@ -0,0 +1,11 @@
+function exemploVar(){
+    if(true){
+        //definir as variaveis
+        var x=10;
+        let y=20;
+        const z=30;
+    }
+    console.log(x); //acessivel
+    console.log(y); //Erro: y não esta definido
+    console.log(z); //Erro: z não esta definido
+}
